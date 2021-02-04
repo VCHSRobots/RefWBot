b33eb0f866b7116b19140d48dde47c1d5e124f5e
DalBrandon <dalbrandon@gmail.com>
2021-02-04 22:07:20 +0000

    initial software

---
+++ rpi/picmd.py
@@ -0,0 +1,61 @@
+# picmd.py -- simple program to test i2c link between pi and ardunio
+# dlb  Feb 21
+
+from smbus import SMBus
+import time
+
+addr = 0x8 # bus address
+bus = SMBus(1) # indicates /dev/ic2-1
+
+def fourbytestolong(u0, u1, u2, u3):
+	x = (u0 << 24) + (u1 << 16) + (u2 << 8) + u3
+	return x
+
+def print_status():
+	id = bus.read_byte_data(addr, 0)
+	bat = bus.read_byte_data(addr, 1)
+	time.sleep(0.01)
+	u0 = bus.read_byte_data(addr, 2)
+	time.sleep(0.01)
+	u1 = bus.read_byte_data(addr, 3)
+	time.sleep(0.01)
+	u2 = bus.read_byte_data(addr, 4)
+	time.sleep(0.01)
+	u3 = bus.read_byte_data(addr, 5)
+	bat = bat / 10.0
+	tt = fourbytestolong(u3, u2, u1, u0)
+	print("Device Id = %c\n" % id)
+	print("Battery Voltage = %f\n" % bat)
+	print("Device Time = %d\n" % tt)
+
+numb = 1
+print("Program to Test Ardunio/Pi I2C Interface")
+print("Menu: ")
+print("  x -- exit the program")
+print("  s -- get status")
+print("  r -- set reg 8 to 111")
+print("  q -- set reg 8 to 222")
+print("  g -- get contents of reg 8")
+
+while True:
+	cc = input(">>>>  ")
+	if len(cc) <= 0:
+		continue
+	c = ord(cc[0])
+	if c == ord('x'):
+		break
+	if c == ord('g'):
+		y = bus.read_byte_data(addr, 8)
+		print("Registor 8 = %d\n" % y)
+		yy = bus.read_byte(addr)
+		print("Second read = %d\n" % yy)
+	if c == ord('r'):
+		bus.write_byte_data(addr, 8, 111)
+		print("Wrote 111 to reg 8.\n")
+	if c == ord('q'):
+		bus.write_byte_data(addr, 8, 222)
+		print("Wrote 222 to reg 8.\n")
+	if c == ord('s'):
+		print_status()
+
+
---
+++ rpi/picomtst.py
@@ -0,0 +1,65 @@
+# picomtst.py -- simple program to test the communications between ardunio and pi
+# dlb  Feb 21
+
+from smbus import SMBus
+import random
+import time
+
+addr = 0x8 # bus address of ardunio
+bus = SMBus(1) # indicates /dev/ic2-1
+
+adr_range = [6,7,8,9]  # registers to test
+regs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def readreg(regadr):
+	x = bus.read_byte_data(addr, regadr)
+	#time.sleep(0.01)
+	return x
+
+def writereg(regadr, dat):
+	bus.write_byte_data(addr, regadr, dat)
+	#time.sleep(0.01)
+
+# start with know values
+for a in adr_range:
+	writereg(a, regs[a])
+
+errcount = 0
+ntry = 0
+nworked = 0
+for i in range(1000):
+	ntry += 1
+	# write random data to a random reg and keep track of it
+	ia = random.randint(0,len(adr_range) - 1)
+	a = adr_range[ia]
+	d = random.randint(0, 255)
+	regs[a] = d
+	writereg(a, d)
+	# read a random reg and see if it matchs what we think it should be
+	ia = random.randint(0, len(adr_range) - 1)
+	a = adr_range[ia]
+	dd = readreg(a)
+	if dd != regs[a]:
+		errcount += 1
+		print("Mismatch on try %d. Reg %d. Found %d. Should be %d.\n" % (i, a, dd, regs[a]))
+		fix  = (dd << 1) + 1
+		if fix == regs[a]:
+			#print("Fix Worked");
+			nworked += 1
+
+# last check
+for a in adr_range:
+	dd = readreg(a)
+	if dd != regs[a]:
+		print("Final check failed on reg %d. Found %d. Should be %d.\n" % (a, dd, regs[a]))
+
+if errcount == 0:
+	print("No errors.  Success!!")
+else:
+	print("Finished. Number of Errors after %d trys: %d\n" % (ntry, errcount))
+	print("Final reg state should be: ")
+	for a in adr_range:
+		v = regs[a]
+		print(v)
+	print("\n")
+	print("Number of times the fix worked: %d\n" % nworked)
