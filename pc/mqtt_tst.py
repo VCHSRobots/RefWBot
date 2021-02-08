d48985fff08a9e07c8f47d093d2165ce1ac45459
dalbrandon <dalbrandon@gmail.com>
2021-02-07 22:24:20 -0800

    "mqtt testing"

---
+++ pc/mqtt_tst.py
@@ -0,0 +1,22 @@
+# A test of basic mqtt message subscribe
+# dlb Feb 21
+
+import paho.mqtt.client as mqtt
+
+broker_url = "192.168.3.25"
+broker_port = 1883
+
+def on_connect(client, userdata, flags, rc):
+   print("Connected With Result Code %d" % rc)
+
+def on_message(client, userdata, message):
+   print("Message Recieved: " + message.payload.decode())
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker_url, broker_port)
+
+client.subscribe("test/msg", qos=1)
+
+client.loop_forever()
