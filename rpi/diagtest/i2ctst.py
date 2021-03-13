a406c17a55082d2025feeae4974ba0b6153bc4be
Dal Brandon <dalbrandon@gmail.com>
2021-03-12 17:38:42 -0800

    bug fixes

--- rpi/diagtest/i2ctst.py
+++ rpi/diagtest/i2ctst.py
@@ -3,28 +3,24 @@
 
 import time
 import sys
-import arduino
+import arduino_wb
 import arduino_reg_map as reg
 import random
 
-okay = arduino.init()
-if not okay:
-  print("Init failed.")
-  sys.exit()
+arduino = arduino_wb.Arduino_wb()
 
 data_errcnt = 0
 bus_errcnt = 0
 ncnt = 0
 
-dx0 = dx1 = dx2 = 0
-arduino.writereg(reg.XXX0, dx0)
-arduino.writereg(reg.XXX0, dx1)
-arduino.writereg(reg.XXX0, dx2)
-
 wregs = [reg.PWM9, reg.PWM10, reg.PWM11, reg.XXX0, reg.XXX1, reg.XXX2]
 wdat  = [0, 0, 0, 0, 0, 0]
 for r in wregs:
-  arduino.writereg(r, 0)
+  try:
+    arduino.writereg(r, 0)
+  except IOError as err:
+    print("Unable to init.  \nErr= ", err)
+    sys.exit()
 
 while True:
   ncnt += 1
@@ -33,8 +29,8 @@ while True:
     r = wregs[ipath]
     wdat[ipath] = d = random.randint(0, 255)
     arduino.writereg(r, d)
-    x = arduino.get_battery_voltage()
-    if x < 9 or x > 12.5:
+    okay, x = arduino.get_battery_voltage()
+    if not okay or x < 9 or x > 12.5:
       data_errcnt += 1
     ipath = random.randint(0, 5)
     r, d = wregs[ipath], wdat[ipath]
@@ -45,6 +41,6 @@ while True:
   except:
     bus_errcnt += 1
     if bus_errcnt < 10: print("Bus error found!")
-  time.sleep(0.020)
+  time.sleep(0.010)
   if ncnt % 50 == 0:
     print("Loop Count: %d,  Bus Errors: %d,  Data Errors: %d" % (ncnt, bus_errcnt, data_errcnt))
