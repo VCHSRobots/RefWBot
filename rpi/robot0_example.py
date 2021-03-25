ed79b1f1cb52b018bfbb097ee289be87477d73df
Dal Brandon <dalbrandon@gmail.com>
2021-03-24 17:13:26 -0700

    Improvements

--- rpi/robot0_example.py
+++ rpi/robot0_example.py
@@ -1,4 +1,4 @@
-# robot0_example.py -- Example user code for a Water Bot
+# robot_ref.py -- Reference Robot -- User Code
 #
 # This file is an example -- You should rename it to something
 # like robot_my_team_name.py, and change the contents for
@@ -9,11 +9,12 @@
 # follow the structure in this example.  That is, you cannot
 # change the class name, and you must provide the functions
 # in the class.
-#
 
 import hydromotor
 import toggle
 import motor775
+import electromagnet
+import limitswitch
 
 class WaterBot():
     def __init__(self, base_sys):
@@ -29,13 +30,17 @@ class WaterBot():
         self.left_motor = hydromotor.HydroMotor(self.base.pca, 4)
         self.right_motor = hydromotor.HydroMotor(self.base.pca, 5)
         self.hydrodrive = hydromotor.HydroDrive(self.left_motor, self.right_motor)
+        self.elecmag = electromagnet.ElectroMagnet(self.base.arduino, 10)
+        self.switch = limitswitch.LimitSwitch(self.base.arduino, 3)
         self.hydrodrive.shutdown()
+        self.elecmag.turn_off()
 
     def stop(self, loop_count):
         ''' Called repeatedly when the robot is in stop mode.'''
         if loop_count == 0:
           print("**** Switching to STOP")
           self.base.pca.killall()
+          self.elecmag.turn_off()
 
     def auto(self, loop_count):
         ''' Called repeatedly when the robot is in auto mode.
@@ -44,6 +49,7 @@ class WaterBot():
         if loop_count == 0:
           print("**** Switching to AUTO")
           self.base.pca.killall()
+          self.elecmag.turn_off()
     
     def teleop(self, loop_count):
         ''' Called repeatedly when the robot is in teleop mode.
@@ -52,6 +58,11 @@ class WaterBot():
         if loop_count == 0:
             print("**** Switching to TELEOP")
             self.hydrodrive.start()
+            self.elecmag.turn_off()
         x, y, z, _, _, _ = self.base.axes0
         self.base.pca.set_servo(15, z)
         self.hydrodrive.move(x, y)
+        if self.switch.get_value():
+          self.elecmag.turn_on()
+        else:
+          self.elecmag.turn_off()
